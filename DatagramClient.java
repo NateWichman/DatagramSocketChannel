2063aba6964d1c43fbd4ac6ffdfc5768f0c3814c
Nathan Cale Wichman <35352401+NateWichman@users.noreply.github.com>
2018-09-20 14:04:55 -0400

    Added notes

--- DatagramClient.java
+++ DatagramClient.java
@@ -1,3 +1,12 @@
+/************************************************************************************
+     udp client. Prompts user for port number, IP address, and a message to send. Then 
+Sends that message to the specified Server. Then attempts to receive a message back 
+from the Server, on which it will print that message out.
+
+@author Nathan Cale Wichman
+@Version September 2018
+************************************************************************************/
+
 import java.io.*;
 import java.net.*;
 import java.nio.*;
@@ -7,30 +16,41 @@ import java.util.*;
 class udpclient{
     public static void main(String args[]){
 	try{
+	    /** Scanner for inputting data **/
 	    Scanner scnr = new Scanner(System.in);
+		
+	    /** Receiving port number from scanner **/
 	    System.out.println("Enter a port number: ");
 	    int portNumber = scnr.nextInt();
+		
+            /** Receiving IP address from Server **/
 	    System.out.println("Enter an IP address: ");
-	    scnr.nextLine();
+	    scnr.nextLine(); //Skipping new line charachter
 	    String ipAddress = scnr.nextLine();
 	    System.out.println("IpReceived: " + ipAddress);
 	    
-	    //Datagram socket. Even though it doesnt have the word socket in it
-	    //Remember a DatagramChannel is a Datagram socket
+	    /** Datagram Socket setup **/
 	    DatagramChannel sc = DatagramChannel.open();
 	    Console cons = System.console();
+		
+	    /** Getting a message to send to the Server from the scanner **/
 	    String m = cons.readLine("Enter your message: ");
+		
+	    /** Sending that message to the Server at the specified ip address and portnumber **/
 	    ByteBuffer buf = ByteBuffer.wrap(m.getBytes());
 	    sc.send(buf, new InetSocketAddress((ipAddress),portNumber));
 
+	    /** Receiving message **/
 	    ByteBuffer buf2 = ByteBuffer.allocate(5000);
 	    sc.receive(buf2);
 	    buf2.flip();
 	    byte[] a = new byte[buf2.remaining()];
 	    buf2.get(a);
 	    String message = new String(buf2.array());
-	    //  String message = new String(buf2.array());
+
 	    System.out.println("Reveived Message from Server: " + message);
+		
+	    /** Closing Socket Channel **/
 	    sc.close();
 
 	}catch(IOException e){
