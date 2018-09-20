7cb4a3f3747cafb92a852b386535f9f4e0d8c622
Nathan Cale Wichman <35352401+NateWichman@users.noreply.github.com>
2018-09-20 14:09:57 -0400

    Added notes

--- DatagramServer.java
+++ DatagramServer.java
@@ -1,3 +1,13 @@
+/***********************************************************************
+     Udp Server. Asks for a port number to intiate receiving of data.
+Listens on that port number for a Client to arrive. Receives a message
+from that Client then sends the same message back. 
+
+Some code provided by proffessor.
+
+@author Nathan Cale Wichman
+@Version September 2018
+***********************************************************************/
 import java.io.*;
 import java.net.*;
 import java.nio.*;
@@ -7,26 +17,28 @@ import java.util.*;
 class udpserver{
     public static void main(String args[]){
 	try{
+	    /** Setting up the scanner and receiving the port number from the user **/
 	    Scanner scnr = new Scanner(System.in);
 	    System.out.println("Enter a port number");
 	    int portNumber = scnr.nextInt();
 	    
+	    /** Setting up the Datagram Socket **/
 	    DatagramChannel c = DatagramChannel.open();
 	    Selector s = Selector.open();
 	    c.configureBlocking(false);
 	    c.register(s,SelectionKey.OP_READ);
 	    c.bind(new InetSocketAddress(portNumber));
+		
+	    /** Searching for a client **/
 	    while(true){
-		/*select checks the channels to see
-		//if we can do the operations.
-		//it returns the numkber or channels
-		//that we can do that operation on */
+		/** Checking for a time out at a specific interval **/
 		int n = s.select(5000);
 		if(n == 0){
 		    System.out.println("got a timeout");
 		}else{
 		    Iterator i = s.selectedKeys().iterator();
 		    while(i.hasNext()){
+			/** Recieving and printing message **/
 			SelectionKey k = (SelectionKey)i.next();
 			ByteBuffer buf = ByteBuffer.allocate(4096);
 			SocketAddress clientaddr = c.receive(buf);
@@ -34,6 +46,7 @@ class udpserver{
 			System.out.println("Received Message: " + message);
 			i.remove();
 			
+			/** Sending Message back to Client **/
 			ByteBuffer buf2 = ByteBuffer.wrap(message.getBytes());
 			c.send(buf2, clientaddr);
 		    }
